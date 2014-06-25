d874f98d638ba5242770d2542459b75c02b7b3dc
David J. Brenes <david.brenes@simplelogica.net>
2014-06-25 15:44:12 +0200

    Slugging the events with friendly_id

--- app/models/no_cms/events/event.rb
+++ app/models/no_cms/events/event.rb
@@ -3,6 +3,10 @@ module NoCms::Events
 
     translates :title, :description, :slug
 
+    extend FriendlyId
+    friendly_id :title, use: [:globalize]
+    include Concerns::GlobalizeSlugs
+
     belongs_to :location, class_name: "NoCms::Events::Location"
 
     scope :future, ->() { where(["starts_at > ? OR ends_at > ?", Time.now, Time.now]) }
--- spec/models/no_cms/events/event_spec.rb
+++ spec/models/no_cms/events/event_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper.rb'
 
 describe NoCms::Events::Event do
   it_behaves_like "model with required attributes", :nocms_events_event, [:title]
+  it_behaves_like "model with slug", :nocms_events_event, :title
 end
