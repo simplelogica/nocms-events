2fa313ea1f87d13152df6f0ea4abd7e5f005f116
David J. Brenes <david.brenes@simplelogica.net>
2014-07-01 11:42:41 +0200

    Nesting modules or the gem can't be properly installed in a new project

--- lib/no_cms/events/engine.rb
+++ lib/no_cms/events/engine.rb
@@ -2,8 +2,10 @@ require 'globalize'
 require 'friendly_id'
 require "friendly_id/globalize"
 
-module NoCms::Events
-  class Engine < ::Rails::Engine
-    isolate_namespace NoCms::Events
+module NoCms
+  module Events
+    class Engine < ::Rails::Engine
+      isolate_namespace NoCms::Events
+    end
   end
 end
